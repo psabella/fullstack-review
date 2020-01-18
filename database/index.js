9303566d88f652d45bb7a83aa90d174cd8216256
Pamela Sabella <psabella@Pamelas-Mac.local>
2020-01-18 10:21:29 -0800

    save data to database with unique records

--- database/index.js
+++ database/index.js
@@ -3,29 +3,23 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 // schema contains properties with data types
 let repoSchema = mongoose.Schema({
-  _id: 'number',
-  name: 'string',
-  url: 'string',
-  watchers_count: 'number',
+  id: {type: Number, unique: true},
+  name: String,
+  url: String,
+  watchers_count: Number,
   owner: {
-    id: 'number',
-    login: 'string'
+    id: Number,
+    login: String
   }
 });
 
 // compiles the schema into a class
 let Repo = mongoose.model('Repo', repoSchema);
 
-// for each repo need to create an instance of the Repo class with the values corresponding to the repo data
-let save = (err, ) => {
-  // TODO: Your code here
-  if (err) {
-    return console.error(err);
-  } else {
-    // TODO
-  }
-  // This function should save a repo or repos to
-  // the MongoDB
+// This function should save a repo or repos to
+// the MongoDB
+let save = (repos) => {
+  Repo.create(repos);
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,37 +1,38 @@
 const express = require('express');
-let app = express();
-let body_parser = require('body-parser');
-let getReposByUsername = require('../helpers/github.js').getReposByUsername;
+const app = express();
+const body_parser = require('body-parser');
+const getReposByUsername = require('../helpers/github.js').getReposByUsername;
+const save = require('../database/index.js');
 
 app.use(body_parser.json());
 app.use(body_parser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+// This route should take the github username provided
+// and get the repo information from the github API, then
+// save the repo information in the database
 app.post('/repos', function (req, res) {
-  // TODO - your code here!
   let username = req.body.term;
   getReposByUsername(username, (err, results) => {
     if (err) {
-      console.err(err);
+      res.status(400).send(err);
     } else {
       let repos = JSON.parse(results.toString());
+      // to check if getting the repos
       repos.forEach((repo) => {
         console.log(repo.name);
-        res.status(201).send('Your Post to Server / Request to API worked');
-        // repo.save()
       })
+      // save the repos to the db
+      save.save(repos);
     }
+  res.status(201).send('Your Post to Server / Request to API worked');
   });
-
-  // This route should take the github username provided
-  // and get the repo information from the github API, then
-  // save the repo information in the database
 });
 
+// This route should send back the top 25 repos
 app.get('/repos', function (req, res) {
   // TODO - your code here!
-  // This route should send back the top 25 repos
 });
 
 let port = 1128;
